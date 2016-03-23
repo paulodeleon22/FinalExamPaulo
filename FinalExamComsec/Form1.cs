6811f0f2729b23f166de244e4934b48560ab65a1
Paowy De Leon <Paowy De Leon>
2016-03-23 11:22:40 +0800

    finals

---
+++ FinalExamComsec/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace FinalExamComsec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Microsoft.Win32.RegistryKey key;
+
+
+        private void writekey(string key , object value)
+        {
+            RegistryKey rk = Registry.CurrentUser;
+            RegistryKey sk1 = rk.CreateSubKey("SOFTWARE\\De Leon");
+            sk1.SetValue(key,value);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writekey("MyFullname","De Leon, Paulo Ross");
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writekey("MyFullname", Encrypt.EncryptString("Paulo Ross De Leon"));          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
